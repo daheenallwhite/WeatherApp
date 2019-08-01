b145ab28dad380899c85400c65b038332f5acd08
Daheen Lee <allwhite.dev@gmail.com>
2019-08-02 03:22:25 +0900

    feat: Add HourlyCollectionViewCell class inherited from UICollectionViewCell class with labels and image view outlet properties

---
+++ WeatherApp/WeatherApp/HourlyCollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  HourlyCollectionViewCell.swift
+//  WeatherApp
+//
+//  Created by Daheen Lee on 02/08/2019.
+//  Copyright © 2019 allwhite. All rights reserved.
+//
+
+import UIKit
+
+class HourlyCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var hourLabel: UILabel!
+    @IBOutlet weak var weatherIconImageView: UIImageView!
+    @IBOutlet weak var temperatureLabel: UILabel!
+}
