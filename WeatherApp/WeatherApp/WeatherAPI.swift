3049e52ce436e1298817f7903792b1270c9cb8eb
Daheen Lee <allwhite.dev@gmail.com>
2019-08-02 03:07:52 +0900

    refactor: Change currentWeatherURL property of WeatherAPI

--- WeatherApp/WeatherApp/WeatherAPI.swift
+++ WeatherApp/WeatherApp/WeatherAPI.swift
@@ -9,6 +9,6 @@
 import Foundation
 
 class WeatherAPI {
-    static let currentWeatherURL = "http://api.openweathermap.org/data/2.5/weather"
+    static let currentWeatherURL = "https://api.openweathermap.org/data/2.5/weather"
     static let appID = "166385d107df2e4cd08f6f7a2973878a"
 }
