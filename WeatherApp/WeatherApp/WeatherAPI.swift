0c8225bfaf16a36e25a3d455db80dbc6bd5ec7c4
Daheen Lee <allwhite.dev@gmail.com>
2019-08-02 02:55:39 +0900

    Add WeatherAPI class with weather api url and app id

---
+++ WeatherApp/WeatherApp/WeatherAPI.swift
@@ -0,0 +1,14 @@
+//
+//  WeatherAPI.swift
+//  WeatherApp
+//
+//  Created by Daheen Lee on 02/08/2019.
+//  Copyright © 2019 allwhite. All rights reserved.
+//
+
+import Foundation
+
+class WeatherAPI {
+    static let currentWeatherURL = "http://api.openweathermap.org/data/2.5/weather"
+    static let appID = "166385d107df2e4cd08f6f7a2973878a"
+}
