50c7bc248be00f15665cb0bc744e9c3e69209c08
Daheen Lee <allwhite.dev@gmail.com>
2019-08-02 02:52:49 +0900

    feat: Add conformance of CLLocationManagerDelegate protocol in ViewController to use most accurate location of user device

--- WeatherApp/WeatherApp/ViewController.swift
+++ WeatherApp/WeatherApp/ViewController.swift
@@ -24,3 +24,23 @@ class ViewController: UIViewController {
 
 }
 
+extension ViewController: CLLocationManagerDelegate {
+    // when it's done updating current location of device
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        //when locationManager finds location information
+        
+        // 마지막 element가 가장 정확도가 높은 위치
+        let location = locations[locations.count-1]
+        
+        // stop updating when you got a valid result
+        if location.horizontalAccuracy > 0 {
+            locationManager.stopUpdatingLocation()
+            
+            let latitude = String(location.coordinate.latitude)
+            let longtitude = String(location.coordinate.longitude)
+            print("longitude = \(longtitude), latitude = \(latitude)")
+            
+            
+        }
+    }
+}
