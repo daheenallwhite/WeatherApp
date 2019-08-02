46535068573d291f41b9a07b2c7c23ecef716564
Daheen Lee <allwhite.dev@gmail.com>
2019-08-02 18:49:28 +0900

    Add DateConverter class for converting date-time string to Date instance

---
+++ WeatherApp/WeatherApp/DateConverter.swift
@@ -0,0 +1,17 @@
+//
+//  DateConverter.swift
+//  WeatherApp
+//
+//  Created by Daheen Lee on 02/08/2019.
+//  Copyright © 2019 allwhite. All rights reserved.
+//
+
+import Foundation
+
+class DateConverter {
+    static func getDate(from string: String) -> Date {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
+        return formatter.date(from: string)
+    }
+}
