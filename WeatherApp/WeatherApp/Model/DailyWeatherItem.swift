91237049f118a560a0b3c9f0fd3937d36d0e907c
Daheen Lee <allwhite.dev@gmail.com>
2019-08-02 23:47:16 +0900

    feat: Add DailyWeatherItem class for representing daily weather forecast data

---
+++ WeatherApp/WeatherApp/Model/DailyWeatherItem.swift
@@ -0,0 +1,37 @@
+//
+//  DailyWeather.swift
+//  WeatherApp
+//
+//  Created by Daheen Lee on 02/08/2019.
+//  Copyright © 2019 allwhite. All rights reserved.
+//
+
+import UIKit
+
+class DailyWeatherItem: WeatherPresentable {
+    var icon: UIImage {
+        return UIImage(named: self.iconName) ?? UIImage()
+    }
+    
+    var temperatureText: String {
+        let max = "\(maxTemperature.toCelcius)"
+        let min = "\(minTemperature.toCelcius)"
+        return "\(max)  \(min)"
+    }
+    
+    var dateText: String {
+        return DateConverter.getDayOfWeek(from: self.date)
+    }
+    
+    private let maxTemperature: Temperature
+    private let minTemperature: Temperature
+    private let date: Date
+    private let iconName: String
+    
+    init(iconName: String, date: String, maxTemperature: Double, minTemperature: Double) {
+        self.maxTemperature = Temperature(kelvin: maxTemperature)
+        self.minTemperature = Temperature(kelvin: minTemperature)
+        self.iconName = iconName
+        self.date = DateConverter.getDate(from: date)
+    }
+}
