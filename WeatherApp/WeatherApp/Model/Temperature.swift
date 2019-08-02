e618c75054787b08833bce63a59d1a93d655b2ff
Daheen Lee <allwhite.dev@gmail.com>
2019-08-02 20:12:27 +0900

    feat: Add Temperature class for managing temperature value

---
+++ WeatherApp/WeatherApp/Model/Temperature.swift
@@ -0,0 +1,27 @@
+//
+//  Temperature.swift
+//  WeatherApp
+//
+//  Created by Daheen Lee on 02/08/2019.
+//  Copyright © 2019 allwhite. All rights reserved.
+//
+
+import Foundation
+
+class Temperature {
+    static let celciusGap = 273.15
+    static let fahrenheitGap = 459.67
+    let kelvinValue: Double
+    
+    init(kelvin: Double) {
+        self.kelvinValue = kelvin
+    }
+    
+    var toCelcius: Int {
+        return Int(kelvinValue - Temperature.celciusGap)
+    }
+    
+    var toFahrenheit: Int {
+        return Int(kelvinValue - Temperature.fahrenheitGap)
+    }
+}
