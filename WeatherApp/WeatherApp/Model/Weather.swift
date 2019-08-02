79c7dc73dc6d07a7a2c0aa08307903b4b444ffd1
Daheen Lee <allwhite.dev@gmail.com>
2019-08-02 18:47:31 +0900

    Add Weather struct for representing data from api

---
+++ WeatherApp/WeatherApp/Model/Weather.swift
@@ -0,0 +1,105 @@
+//
+//  Weather.swift
+//  WeatherApp
+//
+//  Created by Daheen Lee on 02/08/2019.
+//  Copyright © 2019 allwhite. All rights reserved.
+//
+
+import Foundation
+
+import Foundation
+
+// MARK: - Weather
+struct Weather: Codable {
+    let cod: String
+    let message: Double
+    let cnt: Int
+    let list: [List]
+    let city: City
+}
+
+// MARK: - City
+struct City: Codable {
+    let id: Int
+    let name: String
+    let coord: Coord
+    let country: String
+    let population, timezone: Int
+}
+
+// MARK: - Coord
+struct Coord: Codable {
+    let lat, lon: Double
+}
+
+// MARK: - List
+struct List: Codable {
+    let dt: Int
+    let main: MainClass
+    let weather: [WeatherElement]
+    let clouds: Clouds
+    let wind: Wind
+    let sys: Sys
+    let dtTxt: String
+    
+    var date: Date {
+        return DateConverter.getDate(from: dtTxt)
+    }
+    
+    enum CodingKeys: String, CodingKey {
+        case dt, main, weather, clouds, wind, sys
+        case dtTxt = "dt_txt"
+    }
+}
+
+// MARK: - Clouds
+struct Clouds: Codable {
+    let all: Int
+}
+
+
+// MARK: - MainClass
+struct MainClass: Codable {
+    let temp, tempMin, tempMax, pressure: Double
+    let seaLevel, grndLevel: Double
+    let humidity: Int
+    let tempKf: Double
+    
+    enum CodingKeys: String, CodingKey {
+        case temp
+        case tempMin = "temp_min"
+        case tempMax = "temp_max"
+        case pressure
+        case seaLevel = "sea_level"
+        case grndLevel = "grnd_level"
+        case humidity
+        case tempKf = "temp_kf"
+    }
+}
+
+// MARK: - Sys
+struct Sys: Codable {
+    let pod: Pod
+}
+
+enum Pod: String, Codable {
+    case d = "d"
+    case n = "n"
+}
+
+
+// MARK: - WeatherElement
+struct WeatherElement: Codable {
+    let id: Int
+    let main: String
+    let weatherDescription: String
+    let icon: String
+}
+
+
+// MARK: - Wind
+struct Wind: Codable {
+    let speed, deg: Double
+}
+
