0777317921b941e03f537efe5320cd254f310d13
Daheen Lee <allwhite.dev@gmail.com>
2019-08-03 00:01:34 +0900

    feat: Add forecastURL type property to WeatherAPI

--- WeatherApp/WeatherApp/WeatherAPI.swift
+++ WeatherApp/WeatherApp/WeatherAPI.swift
@@ -9,6 +9,7 @@
 import Foundation
 
 class WeatherAPI {
+    static let forecastURL = "https://api.openweathermap.org/data/2.5/forecast"
     static let currentWeatherURL = "https://api.openweathermap.org/data/2.5/weather"
     static let appID = "166385d107df2e4cd08f6f7a2973878a"
 }
