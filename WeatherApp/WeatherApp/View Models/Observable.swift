a9a1497fb039a86cb112cd31763764269163f0c3
Daheen Lee <allwhite.dev@gmail.com>
2019-08-04 00:26:13 +0900

    feat: Add Observable class to implement observer pattern data for view model

---
+++ WeatherApp/WeatherApp/View Models/Observable.swift
@@ -0,0 +1,30 @@
+//
+//  Observable.swift
+//  WeatherApp
+//
+//  Created by Daheen Lee on 03/08/2019.
+//  Copyright © 2019 allwhite. All rights reserved.
+//
+
+import Foundation
+
+
+class Observable<T> {
+    typealias Observer = (T) -> Void
+    var observer: Observer?
+    
+    func observe(_ observer: Observer?) {
+        self.observer = observer
+        observer?(value)
+    }
+    
+    var value: T {
+        didSet {
+            observer?(value)
+        }
+    }
+    
+    init(_ value: T) {
+        self.value = value
+    }
+}
