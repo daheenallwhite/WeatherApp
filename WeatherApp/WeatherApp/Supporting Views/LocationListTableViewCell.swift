71e31f9f8f2d80ea67a41e588cfe14bc8f3b974e
Daheen Lee <allwhite.dev@gmail.com>
2019-08-04 03:26:08 +0900

    Add LocationListTableViewCell class and connect it to main.storyboard

---
+++ WeatherApp/WeatherApp/Supporting Views/LocationListTableViewCell.swift
@@ -0,0 +1,25 @@
+//
+//  LocationListTableViewCell.swift
+//  WeatherApp
+//
+//  Created by Daheen Lee on 04/08/2019.
+//  Copyright © 2019 allwhite. All rights reserved.
+//
+
+import UIKit
+
+class LocationListTableViewCell: UITableViewCell {
+    static let identifier = "LocationListTableViewCell"
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
