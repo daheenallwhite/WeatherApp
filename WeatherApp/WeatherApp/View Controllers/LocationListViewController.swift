cac7078cfe5a92780a9bb60fd3bbb1da72d17843
Daheen Lee <allwhite.dev@gmail.com>
2019-08-04 03:24:31 +0900

    Add LocationViewController class and connect it to scene in main.storyboard

---
+++ WeatherApp/WeatherApp/View Controllers/LocationListViewController.swift
@@ -0,0 +1,30 @@
+//
+//  LocationListViewController.swift
+//  WeatherApp
+//
+//  Created by Daheen Lee on 04/08/2019.
+//  Copyright © 2019 allwhite. All rights reserved.
+//
+
+import UIKit
+
+class LocationListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
