4027e93c5c082ae087e37d2f9ea837544dbd6327
Daheen Lee <allwhite.dev@gmail.com>
2019-08-05 02:03:06 +0900

    feat: Create Coordinate class with initializer using CLLocation, CLLocationCoordinate2D and string pair and make it conform to Equatable protocol

--- WeatherApp/WeatherApp/Extensions/Coordinate.swift
+++ WeatherApp/WeatherApp/Extensions/Coordinate.swift
@@ -9,15 +9,39 @@
 import Foundation
 import CoreLocation
 
-typealias Coordinate = (latitude: String, longitude: String)
-
-extension CLLocationCoordinate2D {
-    func getCoordinatePairString() -> Coordinate {
+class Coordinate {
+    let latitude: String
+    let longitude: String
+    
+    init(location: CLLocation) {
         let maxLength = 10
-        let latitude = String(self.latitude)
-        let longitude = String(self.longitude)
+        let latitude = String(location.coordinate.latitude)
+        let longitude = String(location.coordinate.longitude)
         let lengthModifiedLatitude = latitude.prefix(latitude.count > maxLength ? maxLength : latitude.count)
         let lengthModifiedlongitude = longitude.prefix(longitude.count > maxLength ? maxLength : longitude.count)
-        return (String(lengthModifiedLatitude), String(lengthModifiedlongitude))
+        self.latitude = String(lengthModifiedLatitude)
+        self.longitude =  String(lengthModifiedlongitude)
+    }
+    
+    init(coordinate: CLLocationCoordinate2D) {
+        self.latitude = String(coordinate.latitude)
+        self.longitude =  String(coordinate.longitude)
+    }
+    
+    init(lat: String, lon: String) {
+        self.latitude = lat
+        self.longitude = lon
+    }
+}
+
+extension Coordinate: CustomStringConvertible {
+    var description: String {
+        return "lat: \(self.latitude), lon: \(self.longitude)"
+    }
+}
+
+extension Coordinate: Equatable {
+    static func == (lhs: Coordinate, rhs: Coordinate) -> Bool {
+        return lhs.latitude == rhs.latitude && lhs.longitude == rhs.longitude
     }
 }
