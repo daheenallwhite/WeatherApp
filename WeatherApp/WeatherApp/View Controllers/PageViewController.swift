2bc3a8a8db712de3cc8e89f59fb997e893e877b4
Daheen Lee <allwhite.dev@gmail.com>
2019-08-05 00:43:18 +0900

    feat: Add PageViewController class inherited from UIPageViewController

---
+++ WeatherApp/WeatherApp/View Controllers/PageViewController.swift
@@ -0,0 +1,30 @@
+//
+//  PageViewController.swift
+//  WeatherApp
+//
+//  Created by Daheen Lee on 05/08/2019.
+//  Copyright © 2019 allwhite. All rights reserved.
+//
+
+import UIKit
+
+class PageViewController: UIPageViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
