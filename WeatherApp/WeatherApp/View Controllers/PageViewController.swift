9304378c00966ba6baefe02d18269758c06b9cd3
Daheen Lee <allwhite.dev@gmail.com>
2019-08-05 02:04:09 +0900

    feat: Add page control property and methods for configuring it in PageViewController

--- WeatherApp/WeatherApp/View Controllers/PageViewController.swift
+++ WeatherApp/WeatherApp/View Controllers/PageViewController.swift
@@ -10,10 +10,20 @@ import UIKit
 
 class PageViewController: UIPageViewController {
 
+    var pageControl = UIPageControl()
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+    func configurePageControl() {
+        // The total number of pages that are available is based on how many available colors we have.
+        pageControl = UIPageControl(frame: CGRect(x: 0,y: UIScreen.main.bounds.maxY - 50,width: UIScreen.main.bounds.width,height: 50))
+        self.pageControl.numberOfPages = locationList.count
+        self.pageControl.currentPage = 0
+        self.pageControl.tintColor = UIColor.gray
+        self.pageControl.pageIndicatorTintColor = UIColor.gray
+        self.pageControl.currentPageIndicatorTintColor = UIColor.white
+        self.view.addSubview(pageControl)
     }
     
 
