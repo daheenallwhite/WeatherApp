e9d8b65aa5cb2140aeebe6cb30ab029505465d85
Daheen Lee <allwhite.dev@gmail.com>
2019-08-06 01:15:04 +0900

    feat: Implement caching WeatherViewController instances in PageViewController

--- WeatherApp/WeatherApp/View Controllers/PageViewController.swift
+++ WeatherApp/WeatherApp/View Controllers/PageViewController.swift
@@ -42,25 +42,24 @@ class PageViewController: UIViewController {
         }
         configurePageViewController()
         configurePageControl()
-        configureLocationListButton()
-        setViewControllersForPageViewController(index: 0)
+        configureListButton()
     }
     
-    private func configurePageViewController() {
+    func configurePageViewController() {
         self.pageViewController.delegate = self
         self.pageViewController.dataSource = self
         self.addChild(self.pageViewController)
         self.view.addSubview(self.pageViewController.view)
-        // Set the page view controller's bounds using an inset rect so that self's view is visible around the edges of the pages.
-        let pageViewRect = self.view.bounds
-        let weatherViewRect = CGRect(x: 0, y: 0, width: pageViewRect.width, height: pageViewRect.height - 50)
+        let safeAreaRect = self.view.safeAreaLayoutGuide.layoutFrame
+        let weatherViewRect = CGRect(x: 0, y: 0, width: safeAreaRect.width, height: safeAreaRect.height - 50)
         self.pageViewController.view.frame = weatherViewRect
         self.pageViewController.didMove(toParent: self)
     }
-
-    private func configurePageControl() {
-        pageControl = UIPageControl(frame: CGRect(x: 0,y: UIScreen.main.bounds.maxY - 50,width: UIScreen.main.bounds.width,height: 50))
-        self.pageControl.numberOfPages = locationList.count
+    
+    func configurePageControl() {
+        let safeAreaRect = self.view.safeAreaLayoutGuide.layoutFrame
+        pageControl = UIPageControl(frame: CGRect(x: 0,y: safeAreaRect.maxY - 50,width: safeAreaRect.maxX, height: 50))
+        self.pageControl.numberOfPages = userLocationList.count
         self.pageControl.currentPage = 0
         self.pageControl.tintColor = .gray
         self.pageControl.pageIndicatorTintColor = .gray
@@ -70,8 +69,9 @@ class PageViewController: UIViewController {
         pageControl.addTarget(self, action: #selector(self.changeCurrentPageViewController), for: .valueChanged)
     }
     
-    private func configureLocationListButton() {
-        let buttonRect = CGRect(x: UIScreen.main.bounds.maxX - 50, y: UIScreen.main.bounds.maxY - 40, width: 20, height: 20)
+    func configureListButton() {
+        let safeAreaRect = self.view.safeAreaLayoutGuide.layoutFrame
+        let buttonRect = CGRect(x: safeAreaRect.maxX - 50, y: safeAreaRect.maxY - 40, width: 20, height: 20)
         let locationListButton = UIImageView(frame: buttonRect)
         locationListButton.image = UIImage(named: "list-icon")
         locationListButton.isUserInteractionEnabled = true
@@ -81,15 +81,9 @@ class PageViewController: UIViewController {
         self.view.addSubview(locationListButton)
     }
     
-    
-    private func setViewControllersForPageViewController(index: Int) {
-        let weatherViewController = mainStroryboard.instantiateViewController(withIdentifier: "WeatherViewController") as! WeatherViewController
-        weatherViewController.coordinate = locationList[index]
-        self.pageViewController.setViewControllers([weatherViewController], direction: .forward, animated: false, completion: {done in })
-    }
-    
     @objc func changeCurrentPageViewController() {
-        setViewControllersForPageViewController(index: self.pageControl.currentPage)
+        let pickedViewController = weatherViewController(at: self.pageControl.currentPage)
+        self.pageViewController.setViewControllers([pickedViewController], direction: .forward, animated: false, completion: {done in })
     }
     
     @objc func goToLocationList() {
@@ -125,50 +119,45 @@ extension PageViewController: CLLocationManagerDelegate {
     }
 }
 
+//MARK: page view controller delegate
 extension PageViewController: UIPageViewControllerDelegate {
     func pageViewController(_ pageViewController: UIPageViewController, didFinishAnimating finished: Bool, previousViewControllers: [UIViewController], transitionCompleted completed: Bool) {
         let pageContentViewController = pageViewController.viewControllers![0] as! WeatherViewController
-        self.pageControl.currentPage = locationList.firstIndex(of: pageContentViewController.coordinate!) ?? 0
+        self.pageControl.currentPage = pageContentViewController.index
     }
 }
 
+
+//MARK: page view controller data source
 extension PageViewController: UIPageViewControllerDataSource {
-    private func viewControllerAtIndex(_ index: Int) -> WeatherViewController? {
-        if (self.locationList.count == 0) || (index >= self.locationList.count) {
-            return nil
+    func weatherViewController(at index:Int) -> UIViewController {
+        if let cachedPageViewController = cachedWeatherViewControllers[index] {
+            return cachedPageViewController
         }
-        let weatherViewController = mainStroryboard.instantiateViewController(withIdentifier: "WeatherViewController") as! WeatherViewController
-        weatherViewController.coordinate = self.locationList[index]
-        return weatherViewController
+        guard let pageViewController = mainStroryboard.instantiateViewController(withIdentifier: "WeatherViewController") as? WeatherViewController else {
+            fatalError("WeatherViewcontroller....error")
+        }
+        pageViewController.location = userLocationList[index]
+        pageViewController.index = index
+        cachedWeatherViewControllers[index] = pageViewController
+        return pageViewController
     }
-    private func indexOfViewController(_ viewController: WeatherViewController) -> Int {
-        // Return the index of the given data view controller.
-        // For simplicity, this implementation uses a static array of model objects and the view controller stores the model object; you can therefore use the model object to identify the index.
-        return locationList.firstIndex(of: viewController.coordinate!) ?? NSNotFound
-    }
+    
     func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
-        var index = self.indexOfViewController(viewController as! WeatherViewController)
-        if (index == 0) || (index == NSNotFound) {
-            return nil
+        if let viewController = viewController as? WeatherViewController {
+            let currentIndex = viewController.index
+            return currentIndex > 0 ? weatherViewController(at: currentIndex - 1) : nil
         }
-        
-        index -= 1
-        return self.viewControllerAtIndex(index)
+        return nil
     }
     
     func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
-        var index = self.indexOfViewController(viewController as! WeatherViewController)
-        if index == NSNotFound {
-            return nil
+        if let viewController = viewController as? WeatherViewController {
+            let currentIndex = viewController.index
+            return currentIndex < pageControl.numberOfPages - 1 ? weatherViewController(at: currentIndex + 1) : nil
         }
-        
-        index += 1
-        if index == self.locationList.count {
-            return nil
-        }
-        return self.viewControllerAtIndex(index)
+        return nil
     }
-    
-    
 
 }
+
