47e19d04e0664e3504f74f6bbc6fe501c9497a8e
Daheen Lee <allwhite.dev@gmail.com>
2019-08-06 07:30:31 +0900

    feat: Define WindDirection class for converting wind degree to compass direction

---
+++ WeatherApp/WeatherApp/Model/WindDirection.swift
@@ -0,0 +1,26 @@
+//
+//  WindDirection.swift
+//  WeatherApp
+//
+//  Created by Daheen Lee on 06/08/2019.
+//  Copyright © 2019 allwhite. All rights reserved.
+//
+
+import Foundation
+
+enum WindDirection: Int {
+    case n = 0, nne, ne
+    case ene, e, ese
+    case se, sse, s
+    case ssw, sw, wsw, w
+    case wnw, nw, nnw
+    
+    init(degree: Double) {
+        let convertedValue = Int((degree - 22.5) + 0.5)
+        self = WindDirection(rawValue: convertedValue % 16) ?? .n
+    }
+    
+    var text: String {
+        return "\(self)".uppercased()
+    }
+}
