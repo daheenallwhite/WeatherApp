774377451f3ba1cf39ff5772bd9a3f209ee9fff5
Daheen Lee <allwhite.dev@gmail.com>
2019-08-07 05:26:26 +0900

    feat: Extends StringInterpolation for getting string for each weather value with its symbol

---
+++ WeatherApp/WeatherApp/Extensions/StringInterpolation.swift
@@ -0,0 +1,34 @@
+//
+//  StringInterpolation.swift
+//  WeatherApp
+//
+//  Created by Daheen Lee on 07/08/2019.
+//  Copyright © 2019 allwhite. All rights reserved.
+//
+
+import Foundation
+
+extension String.StringInterpolation {
+    mutating func appendInterpolation(temperature value: Temperature) {
+        appendInterpolation("\(value.text) \(UnitSymbol.forTemperature)")
+    }
+    
+    mutating func appendInterpolation(pressure value: Double) {
+        let convertedPressure = "\(Int(value))"
+        appendInterpolation("\(convertedPressure) \(UnitSymbol.forPressure)")
+    }
+    
+    mutating func appendInterpolation(clouds value: Clouds) {
+        appendInterpolation("\(value.all) \(UnitSymbol.forClouds)")
+    }
+    
+    mutating func appendInterpolation(wind value: Wind) {
+        let directionText = "\(value.direction)".uppercased()
+        appendInterpolation("\(directionText) \(Int(value.deg)) \(UnitSymbol.forWindDegree)")
+    }
+    
+    mutating func appendInterpolation(humidity value: Int) {
+        appendInterpolation("\(value) \(UnitSymbol.forHumidity)")
+    }
+}
+
