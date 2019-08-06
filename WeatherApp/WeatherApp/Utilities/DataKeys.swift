d96d0de829477608bc6877f77a44e8ca3ee6372c
Daheen Lee <allwhite.dev@gmail.com>
2019-08-07 05:32:06 +0900

    feat: Add DataKeys for storing key string to be used when fetching data from UserDefaults

---
+++ WeatherApp/WeatherApp/Utilities/DataKeys.swift
@@ -0,0 +1,16 @@
+//
+//  DataKeys.swift
+//  WeatherApp
+//
+//  Created by Daheen Lee on 07/08/2019.
+//  Copyright © 2019 allwhite. All rights reserved.
+//
+
+import Foundation
+
+struct DataKeys {
+    static let locations = "Locations"
+    static let locationCount = "LocationCount"
+    static let currentPage = "CurrentPage"
+    static let temperatureUnit = "TemperatureUnit"
+}
