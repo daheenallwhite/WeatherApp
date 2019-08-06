f2fd4418e88b541a323e6c7b6a7ea69ee1c3ff9f
Daheen Lee <allwhite.dev@gmail.com>
2019-08-07 05:25:21 +0900

    feat: Add UnitSymbol for getting symbol for weather values

---
+++ WeatherApp/WeatherApp/Utilities/UnitSymbol.swift
@@ -0,0 +1,17 @@
+//
+//  UnitSymbol.swift
+//  WeatherApp
+//
+//  Created by Daheen Lee on 07/08/2019.
+//  Copyright © 2019 allwhite. All rights reserved.
+//
+
+import Foundation
+
+struct UnitSymbol {
+    static let forClouds = "%"
+    static let forWindDegree = "m/s"
+    static let forPressure = "hPa"
+    static let forTemperature = "º"
+    static let forHumidity = "%"
+}
