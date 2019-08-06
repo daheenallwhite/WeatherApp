edbdbd7a13adb17a433e239d485bdf08be981c08
Daheen Lee <allwhite.dev@gmail.com>
2019-08-07 03:23:44 +0900

    refactor: Make DetailWeather to return data per row in table

--- WeatherApp/WeatherApp/Model/DetailWeather.swift
+++ WeatherApp/WeatherApp/Model/DetailWeather.swift
@@ -9,28 +9,25 @@
 import Foundation
 
 class DetailWeather {
-    let humidity: Int
-    let pressure: Pressure
-    let wind: Wind
-    let clouds: Clouds
-    
-    init(humidity: Int, pressure: Pressure, wind: Wind, clouds: Clouds) {
-        self.humidity = humidity
-        self.pressure = pressure
-        self.wind = wind
-        self.clouds = clouds
-    }
-}
-
-class Pressure {
-    let regular: Int
-    let seaLevel: Int
-    let groundLevel: Int
-    
-    init(regular: Double, seaLevel: Double, groundLevel: Double) {
-        self.regular = Int(regular)
-        self.seaLevel = Int(seaLevel)
-        self.groundLevel = Int(groundLevel)
+    typealias DetailWeatherAtRow = (TitleValuePair, TitleValuePair)
+    typealias TitleValuePair = (title: String, value: String)
+    var titleValuPairs = [TitleValuePair]()
+    var totalRow: Int {
+        return self.titleValuPairs.count / 2
     }
     
+    init(humidity: Int, regularPressure: Double, seaLevelPressure: Double, groundLevelPressure: Double, wind: Wind, clouds: Clouds) {
+        titleValuPairs.append(("humidity", "\(humidity)"))
+        titleValuPairs.append(("pressure", "\(regularPressure)"))
+        titleValuPairs.append(("sea level pressure", "\(seaLevelPressure)"))
+        titleValuPairs.append(("ground level pressure", "\(groundLevelPressure)"))
+        titleValuPairs.append(("wind", "\(wind.speedText)"))
+        titleValuPairs.append(("clouds", "\(clouds.text)"))
+    }
+    
+    func getDetailWeather(at rowIndex: Int) -> DetailWeatherAtRow {
+        let leftItem = titleValuPairs[rowIndex]
+        let rightItem = titleValuPairs[rowIndex + 1]
+        return (leftItem, rightItem)
+    }
 }
