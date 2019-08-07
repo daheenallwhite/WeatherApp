833488329955de53b6b385e224e8a75354cc8d46
Daheen Lee <allwhite.dev@gmail.com>
2019-08-07 09:46:05 +0900

    refactor: Rename currentPage lastViewedPage in DataKeys

--- WeatherApp/WeatherApp/Utilities/DataKeys.swift
+++ WeatherApp/WeatherApp/Utilities/DataKeys.swift
@@ -11,6 +11,6 @@ import Foundation
 struct DataKeys {
     static let locations = "Locations"
     static let locationCount = "LocationCount"
-    static let currentPage = "CurrentPage"
+    static let lastViewedPage = "LastViewedPage"
     static let temperatureUnit = "TemperatureUnit"
 }
