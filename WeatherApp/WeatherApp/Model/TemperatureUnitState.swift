9c51c7428cb6499ff0cff17dfae4e15655461915
Daheen Lee <allwhite.dev@gmail.com>
2019-08-07 09:45:21 +0900

    feat: Add TemperatureUnitState for supplying singleton instance for temperature unit preference of user

---
+++ WeatherApp/WeatherApp/Model/TemperatureUnitState.swift
@@ -0,0 +1,30 @@
+//
+//  TemperatureUnitState.swift
+//  WeatherApp
+//
+//  Created by Daheen Lee on 07/08/2019.
+//  Copyright © 2019 allwhite. All rights reserved.
+//
+
+import Foundation
+
+class TemperatureUnitState {
+    static var shared = TemperatureUnitState()
+    var unit: TemperatureUnit
+    
+    init() {
+        let bool = UserDefaults.standard.bool(forKey: DataKeys.temperatureUnit)
+        self.unit = TemperatureUnit(bool: bool)
+    }
+}
+
+enum TemperatureUnit {
+    case celcius, farenheit
+    init(bool: Bool) {
+        self = bool ? .celcius : .farenheit
+    }
+    
+    var boolValue: Bool {
+        return self == .celcius ? true : false
+    }
+}
