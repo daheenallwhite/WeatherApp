e87401d9cc6cf75944b60d92fe6e2c5d960b2cc4
Daheen Lee <allwhite.dev@gmail.com>
2019-08-13 10:02:43 +0900

    refactor: Define nested type Constants in DetailWeather class for managing string literal

--- WeatherApp/WeatherApp/Model/DetailWeather.swift
+++ WeatherApp/WeatherApp/Model/DetailWeather.swift
@@ -11,18 +11,28 @@ import Foundation
 class DetailWeather {
     typealias DetailWeatherAtRow = (TitleValuePair, TitleValuePair)
     typealias TitleValuePair = (title: String, value: String)
+    
+    private struct Constants {
+        static let humidity = "humidity"
+        static let pressure = "pressure"
+        static let seaLevelPressure = "sea level pressure"
+        static let groundLevelPressure = "ground level pressure"
+        static let wind = "wind"
+        static let clouds = "clouds"
+    }
+    
     var titleValuPairs = [TitleValuePair]()
     var totalRow: Int {
         return self.titleValuPairs.count / 2
     }
     
     init(humidity: Int, regularPressure: Double, seaLevelPressure: Double, groundLevelPressure: Double, wind: Wind, clouds: Clouds) {
-        titleValuPairs.append(("humidity", "\(humidity: humidity)"))
-        titleValuPairs.append(("pressure", "\(pressure: regularPressure)"))
-        titleValuPairs.append(("sea level pressure", "\(pressure: seaLevelPressure)"))
-        titleValuPairs.append(("ground level pressure", "\(pressure: groundLevelPressure)"))
-        titleValuPairs.append(("wind", "\(wind: wind)"))
-        titleValuPairs.append(("clouds", "\(clouds: clouds)"))
+        titleValuPairs.append((Constants.humidity, "\(humidity: humidity)"))
+        titleValuPairs.append((Constants.pressure, "\(pressure: regularPressure)"))
+        titleValuPairs.append((Constants.seaLevelPressure, "\(pressure: seaLevelPressure)"))
+        titleValuPairs.append((Constants.groundLevelPressure, "\(pressure: groundLevelPressure)"))
+        titleValuPairs.append((Constants.wind, "\(wind: wind)"))
+        titleValuPairs.append((Constants.clouds, "\(clouds: clouds)"))
     }
     
     func getDetailWeather(at rowIndex: Int) -> DetailWeatherAtRow {
